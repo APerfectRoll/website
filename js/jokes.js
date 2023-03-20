ba6ed57bd1167aa838a8bfae496832a641edac9b
Nikolai Yanovsky <111723187+APerfectRoll@users.noreply.github.com>
2023-03-20 21:44:58 +0000

    Mon, Mar 20, 2023, 2:44 PM -07:00

--- js/jokes.js
+++ js/jokes.js
@@ -4,5 +4,10 @@ const jokeBtn = document.getElementById('jokeBtn')
 jokeBtn.addEventListener('click', generateJoke)
 
 async function generateJoke(){
-    
+    const response = await fetch('https://icanhazdadjoke.com', {
+        headers: {Accept: 'application/json'}
+    })
+
+    const data = await response.json()
+    jokeEl.innerHTML = data.joke
 }
