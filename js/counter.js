9e03de247ac024e990465cb34ae9246919d72792
Nikolai Yanovsky <111723187+APerfectRoll@users.noreply.github.com>
2023-03-21 21:56:12 +0000

    Tue, Mar 21, 2023, 2:56 PM -07:00

--- js/counter.js
+++ js/counter.js
@@ -3,20 +3,5 @@ const counters = document.querySelectorAll('.counter')
 counters.forEach(counter => {
     counter.innerText = '0'
 
-    const updateCounter = () => {
-        const target = +counter.getAttribute('data-target')
-        const c = +counter.innerText
-        const increment = target / 200
 
-        const increment = 1
-
-        if (c < target) {
-            counter.innerText = `${Math.ceil(c + increment)}`
-            setTimeout(updateCounter, 1)
-        } else{
-            counter.innerText = target
-        }
-    }
-
-    updateCounter()
 })
